721860deb4114c518fb4ada7c5dbcd3d58a36fc1
Mohamedsalahmsa <iqthc0226@gmail.com>
2023-07-17 00:17:43 +0300

    First Submit

---
+++ script.js
@@ -0,0 +1,35 @@
+// acess to the input
+let inp = document.querySelector("#inp")
+let btn = document.querySelector("#btn")
+let validP = document.querySelector("#valid-p")
+let conatainer2 = document.querySelector(".conatainer2")
+let conatainer = document.querySelector(".conatainer")
+let SuccessP = document.querySelector("#Success-P")
+let btn2 = document.querySelector("#btn2")
+/*check the elements accessed
+console.log(inp)
+console.log(btn)*/
+
+if(inp !== "") {
+    btn.addEventListener("click" , function() {
+        let rejexemail = /[a-z](\d+)?@\w+(.\w+)/ig
+        let getValid =  rejexemail.test(inp.value);
+        if(getValid === false) {
+            inp.style.backgroundColor = "rgba(255, 1, 1, 0.315)";
+            inp.style.border = "1px solid rgba(255, 1, 1, 0.315)";
+            inp.style.color = " rgba(255, 1, 1, 1)";
+            validP.style.display = "block";
+        } else if (getValid === true) {
+            conatainer.style.display = "none";
+            conatainer2.style.display = "flex";
+            SuccessP.innerHTML = `A confirmation email has been sent to <br> <span>${inp.value}</span>  
+            Please open it and click <br> the button inside to confirm your subscription.`
+        }
+    })
+}
+
+btn2.addEventListener("click" , function() {
+    conatainer2.style.display = "none";
+    conatainer.style.display = "flex";
+    inp.value = "";
+})
